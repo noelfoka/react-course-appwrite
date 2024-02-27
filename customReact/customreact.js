5bd16ed4a6a6a10ebe8f6e4d349ba2bdbaff8ff7
noelfoka <nomgnenoel@gmail.com>
2024-02-27 11:33:29 +0100

    update

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,10 +1,17 @@
+function mainContainer(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute('href', reactElement.props.href);
+  domElement.setAttribute('target', reactElement.props.target);
+}
+
 const reactElement = {
   type: "a",
   props: {
     href: "http://google.com",
     target: "_blank",
   },
-  cheldren: "Click me to visit google"
+  children: "Click me to visit google"
 }
 
 const mainContainer = document.querySelector("#root");
