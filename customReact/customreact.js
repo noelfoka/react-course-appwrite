7d616e15ddbfc6ee8e52e68234587b5eeea21d7b
noelfoka <nomgnenoel@gmail.com>
2024-02-27 11:34:37 +0100

    update1

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -3,6 +3,7 @@ function mainContainer(reactElement, container) {
   domElement.innerHTML = reactElement.children;
   domElement.setAttribute('href', reactElement.props.href);
   domElement.setAttribute('target', reactElement.props.target);
+  container.appendChild(domElement);
 }
 
 const reactElement = {
