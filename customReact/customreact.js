5e1f660642750ca2230b924d0d64322f5e938ceb
noelfoka <nomgnenoel@gmail.com>
2024-02-27 14:45:41 +0100

    update method

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,9 +1,17 @@
+/*
 function mainContainer(reactElement, container) {
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
   domElement.setAttribute('href', reactElement.props.href);
   domElement.setAttribute('target', reactElement.props.target);
   container.appendChild(domElement);
+}
+*/
+
+const domElement = document.createElement(reactElement.type);
+domElement.innerHTML = reactElement.childen;
+for (const prop in reactElement.props) {
+  domElement.setAttribute(prop, reactElement.props[prop]);
 }
 
 const reactElement = {
@@ -12,7 +20,7 @@ const reactElement = {
     href: "http://google.com",
     target: "_blank",
   },
-  children: "Click me to visit google"
+  childen: "Click me to visit google"
 }
 
 const mainContainer = document.querySelector("#root");
