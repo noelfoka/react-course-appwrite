0ecdb508be91bb87eb14b8c96fe0a253577912ae
noelfoka <nomgnenoel@gmail.com>
2024-02-27 14:51:40 +0100

    update mainContainer

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,16 +1,26 @@
-/*
 function mainContainer(reactElement, container) {
+  /*
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
   domElement.setAttribute('href', reactElement.props.href);
   domElement.setAttribute('target', reactElement.props.target);
   container.appendChild(domElement);
+  */
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.childen;
+  for (const prop in reactElement.props) {
+    if (prop === 'childen') continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
 }
-*/
+
 
 const domElement = document.createElement(reactElement.type);
 domElement.innerHTML = reactElement.childen;
 for (const prop in reactElement.props) {
+  if (prop === 'childen') continue;
   domElement.setAttribute(prop, reactElement.props[prop]);
 }
 
