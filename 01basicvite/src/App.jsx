d72f291b012fd73c8d22091728d3c1d73a2dbdff
noelfoka <nomgnenoel@gmail.com>
2024-02-28 08:12:04 +0100

    create basic vite app to count

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -2,9 +2,13 @@ import Youtube from './youtube';
 
 function App() {
 
+  const username = 'Noel FOKA';
+  const age = 33;
   return (
     <>
-      <h1>Vite React App.</h1>
+      <h1>My name is {username}</h1>
+      <h1>My age is {age}</h1>
+      <h1>{(username && (age === 33)) ? 'yes' : 'no'} </h1>
       <Youtube />
     </>
   )
