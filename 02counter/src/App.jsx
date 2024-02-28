ff3bd3e0c2bd127c55088a3f888311691d296951
noelfoka <nomgnenoel@gmail.com>
2024-02-28 23:44:05 +0100

    update

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,13 +2,18 @@ import './App.css'
 
 function App() {
 
+  let counter = 50;
   return (
     <>
-     <h1>React course with Joel</h1>
-     <h2>Counter value: </h2>
-     <button>Add Value</button>
-     <button>Remove Value</button>
-     <p>Footer: </p>
+      <h1>React course with Joel</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+        onClick={addValue}
+      >
+        Add Value
+      </button> {" "}
+      <button>Remove Value</button>
+      <p>Footer: {counter}</p>
     </>
   )
 }
