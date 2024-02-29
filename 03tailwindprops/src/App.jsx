095ef17790f888baddd1ea8fd2b8a3ffc71ca9a2
noelfoka <nomgnenoel@gmail.com>
2024-02-29 14:51:46 +0100

    update3

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
       <h1 className='text-3xl bg-green-400 p-3 rounded-full'>Vite with tailwind</h1>
+      <Card />
     </>
   )
 }
