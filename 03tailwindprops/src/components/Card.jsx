9c7279e32053f36f0301cb70ff98998e4ff14c67
noelfoka <nomgnenoel@gmail.com>
2024-02-29 14:33:42 +0100

    update2

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,7 +1,13 @@
 function Card() {
   return (
-    <div>Card</div>
-  )
+    <div>
+      <img src="" alt="" />
+      <h1 className="text-2xl bg-green-500 p-3 rounded">
+        Card for photos
+      </h1>
+      <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+    </div>
+  );
 }
 
 export default Card
