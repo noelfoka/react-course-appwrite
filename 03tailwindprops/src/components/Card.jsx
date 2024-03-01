d90a7c584e605c36fe0b7c75801be56ecc133e72
noelfoka <nomgnenoel@gmail.com>
2024-03-01 06:04:55 +0100

    update

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card() {
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
+        “Tailwind CSS is the only framework that I&apos;ve seen scale
         on large teams. It’s easy to customize, adapts to any design,
         and the build size is tiny.”
       </p>
