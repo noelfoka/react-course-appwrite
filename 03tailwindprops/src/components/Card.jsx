fa566113c6f5404e1b3ffd8cb07310903d1a4566
noelfoka <nomgnenoel@gmail.com>
2024-03-01 06:39:34 +0100

    update 2

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,28 +1,29 @@
 import photo1 from "./photo1.jpg";
 
-function Card() {
+function Card(props) {
+  console.log(props);
   return (
     <div>
       <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img className="w-24 h-24 rounded-full mx-auto" src={photo1} alt="" width="384" height="512" />
-  <div className="pt-6 text-center space-y-4">
-    <blockquote>
-      <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption className="font-medium">
-      <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div className="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+        <img className="w-24 h-24 rounded-full mx-auto" src={photo1} alt="" width="384" height="512" />
+        <div className="pt-6 text-center space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I&apos;ve seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              Sarah Dayan
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
     </div>
   );
 }
