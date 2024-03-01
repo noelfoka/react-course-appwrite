6eb9b14930b2cf5f732ef13a1f6b355a57dafc7b
noelfoka <nomgnenoel@gmail.com>
2024-03-01 12:42:32 +0100

    add style

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -5,15 +5,17 @@ function App() {
 
   const [color, setColor] = useState('olive');
   return (
-    <div className="w-full h-screen duration-200">
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
+            onClick={() => setColor('red')}
             className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
-          >Test 1</button>
+          >Red</button>
           <button
+            onClick={() => setColor('green')}
             className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
-          >Test 2</button>
+          >Green</button>
         </div>
       </div>
     </div>
