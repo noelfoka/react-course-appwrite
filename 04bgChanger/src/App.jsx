6f2e7a4d99e3cb8e0ad477316f497779fa7f510c
noelfoka <nomgnenoel@gmail.com>
2024-03-01 12:06:07 +0100

    update

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <div className="w-full h-screen duration-200">
+    <div className="w-full h-screen duration-200 bg-black">
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button>Test</button>
+          <button
+            className="outline-none px-4 py-1 round-full text-white shadow-lg"
+          >Test</button>
         </div>
       </div>
     </div>
