08b9df4396b2139864942782f38e6bf9df94da94
noelfoka <nomgnenoel@gmail.com>
2024-03-04 11:41:00 +0100

    implement another input with type range

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
     <h1 className='text-white my-3 text-center'>Password Generator</h1>
+
     <div className='flex shadow rounded-lg overflow-hidden mb-4'>
       <input 
         type='text'
@@ -24,6 +25,21 @@ function App() {
         >
           Copy
       </button>
+    </div>
+
+    <div
+      className='flex text-sm gap-x-2'
+    >
+      <div className='flex items-center gap-x-1'>
+        <input 
+          type='range'
+          min={6}
+          max={100}
+          value={length}
+          name=''
+
+        />
+      </div>
     </div>
    </div>
   )
