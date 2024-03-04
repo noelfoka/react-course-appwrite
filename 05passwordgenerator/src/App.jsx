a3ccc6502c8878a29ca38a4e7026bd2e2f6a6561
noelfoka <nomgnenoel@gmail.com>
2024-03-04 11:06:40 +0100

    update1

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
 
   return (
    <h1 className='bg-red-400'>Setup started</h1>
