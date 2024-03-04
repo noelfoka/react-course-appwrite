f9dcffa82f0f3847cf39c439de6ff8b714cef169
noelfoka <nomgnenoel@gmail.com>
2024-03-04 14:23:03 +0100

    start implement generate password

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -16,8 +16,11 @@ function App() {
     if (charAllowed) str += '!@#$%^&*()_+';
 
     for (let i = 1; i < length; i++) {
-      Math.floor(Math.random() * str.length + 1);
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
+
+    setPassword(pass);
   })
 
   return (
