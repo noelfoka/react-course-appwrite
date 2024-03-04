fc1bb8f3ee910e11d76c7681901c0214792ddbeb
noelfoka <nomgnenoel@gmail.com>
2024-03-04 11:05:24 +0100

    update

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 function App() {
 
   const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
 
   return (
    <h1 className='bg-red-400'>Setup started</h1>
