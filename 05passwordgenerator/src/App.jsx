c18c46b08b0f6646581b3705fc2376e8f66a178a
noelfoka <nomgnenoel@gmail.com>
2024-03-04 11:03:35 +0100

    initialize useState

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [length, setLength] = useState(8);
 
   return (
    <h1 className='bg-red-400'>Setup started</h1>
