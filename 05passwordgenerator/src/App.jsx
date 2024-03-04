5800fda645ef2078ddc1c9f1630775798ba88ed3
noelfoka <nomgnenoel@gmail.com>
2024-03-04 11:13:54 +0100

    add title

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -9,7 +9,9 @@ function App() {
   const [password, setPassword] = useState('');
 
   return (
-   <h1 className='bg-red-400'>Setup started</h1>
+   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+    <h1 className='text-3xl font-bold mb-2 text-center'>Password Generator</h1>
+   </div>
   )
 }
 
