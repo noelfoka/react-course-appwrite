e7db4470238ccdc545e92497e8254af156da33e9
noelfoka <nomgnenoel@gmail.com>
2024-03-05 08:57:33 +0100

    condition

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   const copyPasswordToClipboard = () => {
     window.navigator.clipboard.writeText(password);
-    passwordRef.current.select();
+    passwordRef.current?.select();
   }
 
   useEffect(() => {
