328ce5a199141578a3d3611dd4b7ff2603bcc7e6
noelfoka <nomgnenoel@gmail.com>
2024-03-05 12:30:05 +0100

    add props

--- 06currencyconverter/src/components/InputBox.jsx
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -1,4 +1,14 @@
-function InputBox() {
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectedCurrency = "usd",
+  amoundDisabled = false,
+  currencyDisabled = false,
+  className = "",
+}) {
   return (
     <div>InputBox</div>
   )
