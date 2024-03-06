1d94c3f63751beb66e9b50500c821c5249a9a88f
noelfoka <nomgnenoel@gmail.com>
2024-03-06 08:23:11 +0100

    add paragraph

--- 06currencyconverter/src/components/InputBox.jsx
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -24,6 +24,9 @@ function InputBox({
           onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
         />
       </div>
+      <div className='w-1/2 flex flex-wrap justify-end text-right'>
+        <p className='text-black-40 mb-2 w-full'>Currency Type</p>
+      </div>
     </div>
   )
 }
