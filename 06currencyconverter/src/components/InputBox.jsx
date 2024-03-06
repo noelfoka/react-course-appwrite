ec920a6de98973ea6e37d83bc647ec0188153915
noelfoka <nomgnenoel@gmail.com>
2024-03-06 15:16:25 +0100

    update

--- 06currencyconverter/src/components/InputBox.jsx
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useId } from 'react';
 
 function InputBox({ 
   label,
@@ -14,10 +14,11 @@ function InputBox({
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className='w-1-2'>
-        <label className='text-black/40 mb-2 inline-block'>
+        <label htmlFor='currency' className='text-black/40 mb-2 inline-block'>
           {label}
         </label>
         <input
+          id='currency'
           type='number'
           className='outline-none bg-transparent py-1.5 w-full'
           placeholder='Amount'
