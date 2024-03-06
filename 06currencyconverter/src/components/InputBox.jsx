250065e304067a9c21989a1511053b8f932945d7
noelfoka <nomgnenoel@gmail.com>
2024-03-06 08:29:58 +0100

    onChange

--- 06currencyconverter/src/components/InputBox.jsx
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -29,6 +29,7 @@ function InputBox({
         <select
           className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
           value={selectedCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
         >
 
         </select>
