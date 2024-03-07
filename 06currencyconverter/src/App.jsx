afb64238193540b59cd6709fb9b28cd9adc83245
noelfoka <nomgnenoel@gmail.com>
2024-03-07 08:11:20 +0100

    add another div

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import InputBox from './components/InputBox'
 import { useState } from 'react';
 import useCurrencyInfo from './hooks/useCurrencyInfo';
 
@@ -29,7 +28,9 @@ function App() {
       className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
       style={{backgroundImage: `url(https://www.cnet.com/a/img/resize/11ecd648ae46d7edad540bf1280a5d6fdac46ffe/hub/2023/02/14/ad269de1-1d9f-4bb1-9b3b-87a9dff38e72/cash-money-fan-100s-teal-spiral.png?auto=webp&fit=crop&height=900&width=1200)`}}
     >
-      <InputBox />
+      <div className='w-full'>
+        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'></div>
+      </div>
     </div>
   )
 }
