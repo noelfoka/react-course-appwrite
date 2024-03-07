6f99351aebacf9d7db7574343cde0b923ce73b3e
noelfoka <nomgnenoel@gmail.com>
2024-03-07 14:34:58 +0100

    update

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -27,10 +27,10 @@ function App() {
   return (
     <div 
       className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
-      style={{backgroundImage: `url(https://www.cnet.com/a/img/resize/11ecd648ae46d7edad540bf1280a5d6fdac46ffe/hub/2023/02/14/ad269de1-1d9f-4bb1-9b3b-87a9dff38e72/cash-money-fan-100s-teal-spiral.png?auto=webp&fit=crop&height=900&width=1200)`}}
+      style={{backgroundImage: `url(https://img.freepik.com/premium-photo/creative-glowing-dollar-hologram-with-metaverse-lines-dark-wide-background-money-online-banking-app-currency-finance-concept-3d-rendering_670147-7824.jpg)`}}
     >
       <div className='w-full'>
-        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/50'>
           <form onSubmit={(e) => {
             e.preventDefault();
             convert();
@@ -48,7 +48,7 @@ function App() {
 
             <div className='w-full relative h-0.5'>
               <button
-                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-full bg-blue-600 px-2 text-white py-0.5 pb-1'
+                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-full bg-blue-900 px-2 text-white py-0.5 pb-1'
                 onClick={swap}
               >Swap</button>
             </div>
@@ -66,7 +66,7 @@ function App() {
 
             <button
               type='submit'
-              className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg'
+              className='w-full bg-blue-900 text-white px-4 py-3 rounded-lg'
             >Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
           </form>
         </div>
