7f7927c8aedddfa686443a3a936675f5705699e5
noelfoka <nomgnenoel@gmail.com>
2024-03-08 14:12:44 +0100

    update1

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
-const router = createBrowserRouter();
+const router = createBrowserRouter(
+  createRoutesFromElements
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
