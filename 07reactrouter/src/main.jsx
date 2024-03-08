5249fedbcac3ad454bda1c907d36fd3783caac55
noelfoka <nomgnenoel@gmail.com>
2024-03-08 14:21:25 +0100

    start implement route

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
 
 const router = createBrowserRouter(
-  createRoutesFromElements
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider />
   </React.StrictMode>,
 )
